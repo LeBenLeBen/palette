e910860ff2657e519ca587930814d8b59cebd609
Benoît Burgener <benoit.burgener@gmail.com>
2022-03-23 18:34:07 +0100

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:LeBenLeBen/palette.git master:gh-pages
+git push -f git@github.com:LeBenLeBen/palette.git main:gh-pages
 
 cd -
